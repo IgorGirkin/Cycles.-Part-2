6b67fab6af985a16bf8b21de1b3815f80cb04d27
grisha <norsulfasol@gmail.com>
2022-10-20 19:49:44 +0300

    Циклы. Часть 2 Домашнее задание -2

--- src/Main.java
+++ src/Main.java
@@ -2,36 +2,39 @@ public class Main {
     public static void main(String[] args) {
         //Задание 1
         System.out.println("Задание 1");
-        int accumulation = 15_000;
-        int total = 0;
-        int month=0;
-        while ( total<=2459000) {
-        total=total+total/100;
-        total=total+accumulation;
-        month=month+1;
-        System.out.println("Месяц "+month+", сумма накоплений равна "+total+"рублей");
+        var deposit = 15000;
+        var total=0;
+        for (int month = 1;total < 12000000;month++) {
+            total+=total/100*7;
+            total+=deposit;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
         }
-        //Задание 2
-        System.out.println("Задание 2");
-        int q=0;
-        while ( q<=9) {
-            q++;
-            System.out.print(" "+q);
-        }
-        System.out.println();
-        for (;q>=1;q--) {
-         System.out.print(" "+q);
-        }
-        System.out.println();
-        //Задание 3
-         System.out.println("Задание 3");
-         var y=12000000;
-         var birthRatePerThousand = 17;
-         var mortalityRatePerThousand = 8;
-         for (int w=1; w<=10;w++) {
-         y=y+(y/1000*birthRatePerThousand)-(y/1000*mortalityRatePerThousand);
-         System.out.println("Год "+w+ ", численность населения составляет "+y);
-         }
-    }
 
+        //Задание 2
+        //   System.out.println("Задание 2");
+        //   var deposit2 = 15000;
+        //  for (int q = 1; deposit2 <= 12000000; q++) {
+        //    deposit2 = deposit2 +percent;
+        //     if (q % 6 == 0) {
+        //            System.out.println("Месяц " + q + ", сумма накоплений равна " + deposit2 + " рублей");
+        //       }
+        //    }
+        //    //Задание 3
+        //  System.out.println("Задание 3");
+        ////   var deposit3 = 15000;
+        //    var period = 12*9;
+        //   for (int w = 1; w <= period; w++) {
+        //      deposit3+=percent;
+        //     if (w % 6 == 0) {
+        //          System.out.println("Месяц " + w + ", сумма накоплений равна " +deposit3+ " рублей");
+        //      }
+        //  }
+        //   //Задание 4
+            System.out.println("Задание 4");
+           int e = 5;
+           for (int j = 1;j<31;j++) if ((j-e)%7==0) {
+             System.out.println("Сегодня пятница, "+j+"-е число. Необходимо подготовить отчет.");
+         }
+
+    }
 }
